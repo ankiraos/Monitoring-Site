32c4d9dbf7eaeb77644a58bf87574f7a1f37ef31
Ankita Joahi <joshiankita5825%@gmail.com>
2024-08-27 21:39:58 +0530

    adding static root in settings file

--- celery_project/settings.py
+++ celery_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from .secret import *
-
+import os
 
 # Application definition
 
@@ -100,6 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
